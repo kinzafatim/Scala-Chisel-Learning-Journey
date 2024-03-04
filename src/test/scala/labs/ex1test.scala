73e9e10211d7c3db2f45b3b9d1b4936ec39a60ad
kinzafatim <kinzaf54@gmail.com>
2024-03-04 14:18:08 +0500

    lab 1 complete

--- src/test/scala/labs/ex1test.scala
+++ src/test/scala/labs/ex1test.scala
@@ -2,7 +2,7 @@ package labs
 import chisel3._
 import chiseltest._
 import org.scalatest._
-class lab1ex1test extends FreeSpec with ChiselScalatestTester{
+class ex1test extends FreeSpec with ChiselScalatestTester{
     "counter1" in {
         test(new counter1(2.S)){
         x=>
