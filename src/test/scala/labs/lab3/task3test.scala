72d2a922045c500c53b02900c65e66417d77797d
kinzafatim <kinzaf54@gmail.com>
2024-03-20 22:47:50 +0500

    Done

--- src/test/scala/labs/lab3/task3test.scala
+++ src/test/scala/labs/lab3/task3test.scala
@@ -6,11 +6,10 @@ class task3test extends FreeSpec with ChiselScalatestTester{
     "Valid Interface" in{
         test(new task3){
             x=>
-            x.io.instr.poke("b0010011".U) 
-           // x.io.opcode.poke(5.U) 
+            x.io.in.poke("b0010011".U) 
             x.clock.step(4)
 
-            x.io.out_ins.expect("b00000000000000000000_1111111111".U)
+            x.io.out.expect(.U)
         }
     }
 }
